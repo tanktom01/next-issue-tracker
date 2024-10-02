a513dabbfbb72b7f65e5a0cc02e1cceb139540e4
Thomas Nowakowski <tom.nav2001@gmail.com>
2024-10-02 10:23:46 +1000

    buiilt new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <Button>
+      <Link href="/issues/new">New Issue</Link>
+    </Button>
+  );
 };
 
 export default IssuesPage;
