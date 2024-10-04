4315f8623eb9f83d1559380093b416c9d94f9443
Thomas Nowakowski <tom.nav2001@gmail.com>
2024-10-04 13:24:36 +1000

    additional loading skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,16 @@
+import { Box } from "@radix-ui/themes";
+import delay from "delay";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max=w-xl">
+      <Skeleton width={"4rem"} />
+      <Skeleton height={"20rem"} />
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import delay from "delay";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
