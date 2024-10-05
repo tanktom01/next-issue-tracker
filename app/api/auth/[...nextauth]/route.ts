99d18fafd9a9e45b8f3f76ca803fcf15dc3254e9
Thomas Nowakowski <tom.nav2001@gmail.com>
2024-10-06 09:03:30 +1100

    configure google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,16 @@
-import { handlers } from "../../../auth";
-export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+});
